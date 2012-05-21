69f10c0ca61c7b8be1c66698e15bd09a48a19471
kayayesildag@gmail.com <kayayesildag@gmail.com>
2012-05-21 11:57:54 +0200

    v3(Devise gefixt en inlog sheize)

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.3'
 
 gem 'sqlite3'
 
+gem 'devise'
+
 gem 'json'
 
 # Gems used only for assets and not required
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Kayaishommoh::Application.routes.draw do
+  devise_for :users
+
   get "home/index"
   root :to => "home#index"
   resources :posts
